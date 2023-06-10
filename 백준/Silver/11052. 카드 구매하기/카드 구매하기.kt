fa67fd2d269e4a6820aed9a30ae817cf936c4eb7
jlkim909 <79569951+jlkim909@users.noreply.github.com>
2023-06-10 18:04:27 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 13680 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.kt
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.kt
@@ -10,10 +10,7 @@ val p2 = Array<Int>(1001){
     0
 }
 
-// f[1] = p[1] = 1
-// f[2] = min(f[1] + p[1], p[2]) = 5
-// f[3] = min(f[2] + p[1], p[3]) = 6
-// f[4] = min(f[3] + p[1], p[4]) = 7 // f[1] + f[3], f[2]+f[2]
+// f[4] = min(f[1] + f[3], f[2] + f[2], p[4])
 // O(1/2*n^2) n<=1000, 1000000
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
@@ -29,7 +26,7 @@ fun main(){
 fun bp(n:Int):Int{
     if(p2[n] > 0) return p2[n]
     var temp = 0
-    for(num in 1 until n){
+    for(num in 1..n/2){
         var temp2 = bp(num)+bp(n-num)
         if(temp < temp2)
             temp = temp2
