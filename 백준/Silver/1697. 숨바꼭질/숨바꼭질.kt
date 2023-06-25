5462fb8dde8051e9cbd39cd9ee6ff0b20696c8e9
jlkim909 <79569951+jlkim909@users.noreply.github.com>
2023-06-25 19:38:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 312 ms, Memory: 26828 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.kt
@@ -0,0 +1,42 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val q = ArrayDeque<Int>()
+    val token = StringTokenizer(br.readLine())
+    val n = token.nextToken().toInt()
+    val k = token.nextToken().toInt()
+    val d = Array<Int>(200000){
+        -1
+    }
+    val MAX = 2*k
+    q.add(n)
+    d[n] = 0
+    while(!q.isEmpty()){
+        val now = q.removeFirst()
+        if(now + 1 < MAX){
+            if(d[now+1] == -1) {
+                q.add(now + 1)
+                d[now + 1] = d[now] + 1
+            }
+        }
+
+        if(now - 1 >= 0){
+            if(d[now-1] == -1) {
+                q.add(now - 1)
+                d[now - 1] = d[now] + 1
+            }
+        }
+
+        if(now * 2 < MAX){
+            if(d[now*2] == -1) {
+                q.add(now*2)
+                d[now*2] = d[now] + 1
+            }
+        }
+    }
+
+    print(d[k])
+}
