1a63f3fd5d41afc4e012fb808ddbc4596b11c4a4
jlkim909 <79569951+jlkim909@users.noreply.github.com>
2024-02-15 01:00:24 +0900

    [Gold III] Title: 줄 세우기, Time: 924 ms, Memory: 53172 KB -BaekjoonHub

---
+++ 백준/Gold III/2252. 줄 세우기/줄 세우기.kt
@@ -0,0 +1,42 @@
+package topologicalSorting
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var t1 = StringTokenizer(br.readLine())
+    val n = t1.nextToken().toInt()
+    val m = t1.nextToken().toInt()
+    val ind = Array(n+1){
+        0
+    }
+    val g = Array(n+1){
+        mutableListOf<Int>()
+    }
+    repeat(m){
+        t1 = StringTokenizer(br.readLine())
+        val f = t1.nextToken().toInt()
+        val s = t1.nextToken().toInt()
+        g[f].add(s)
+        ind[s]++
+    }
+
+    val q = ArrayDeque<Int>()
+    for(i in 1..n){
+        if(ind[i] == 0){
+            q.add(i)
+        }
+    }
+    while(!q.isEmpty()){
+        val t = q.removeFirst()
+        print("$t ")
+        for(num in g[t]){
+            ind[num]--
+            if(ind[num] == 0){
+                q.add(num)
+            }
+        }
+    }
+}
