be11a58d6c329a7cf774e726f71166a9efd6b0a8
jlkim909 <79569951+jlkim909@users.noreply.github.com>
2024-03-10 20:10:03 +0900

    [Silver III] Title: 주유소, Time: 568 ms, Memory: 77796 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.kt
+++ 백준/Silver/13305. 주유소/주유소.kt
@@ -1,12 +1,14 @@
 package greedy
 
+import java.math.BigInteger
+
 fun main() = with(System.`in`.bufferedReader()){
     val n = readLine().toInt()
-    val roads = readLine().split(" ").map{it.toInt()}
-    val price = readLine().split(" ").map{it.toInt()}
+    val roads = readLine().split(" ").map{BigInteger.valueOf(it.toLong())}
+    val price = readLine().split(" ").map{BigInteger.valueOf(it.toLong())}
 
-    var result = 0
-    var maxPrice = Int.MAX_VALUE
+    var result = BigInteger.valueOf(0)
+    var maxPrice = BigInteger.valueOf(1_000_000_001)
     for((idx, road) in roads.withIndex()){
         if(price[idx] < maxPrice){
             maxPrice = price[idx]
