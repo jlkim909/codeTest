7ee39aa7faa094b8efd6ba33d028e09e2e58dd22
hodak <79569951+jlkim909@users.noreply.github.com>
2024-06-04 21:02:40 +0900

    [Gold IV] Title: 여왕벌, Time: 3360 ms, Memory: 120924 KB, Score: 83 point -BaekjoonHub

---
+++ 백준/Gold/10836. 여왕벌/여왕벌.kt
@@ -0,0 +1,40 @@
+package all
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (m, n) = readLine().split(" ").map { it.toInt() }
+    val bees = IntArray(2 * m - 1) { 1 }
+
+    repeat(n) {
+        val (zero, one, two) =
+            readLine().split(" ").map { it.toInt() }
+        var k = zero
+        repeat(one){
+            bees[k++] += 1
+        }
+
+        repeat(two){
+            bees[k++] += 2
+        }
+    }
+
+    val beesList = Array(m){
+        IntArray(m)
+    }
+
+    for(i in 0..<m){
+        beesList[i][0] = bees[m-i-1]
+    }
+
+    for(i in 0..<m){
+        for(j in 1..<m){
+            beesList[i][j] = bees[m+j-1]
+        }
+    }
+
+    for(i in 0..<m){
+        for(j in 0..<m){
+            print("${beesList[i][j]} ")
+        }
+        println()
+    }
+}
