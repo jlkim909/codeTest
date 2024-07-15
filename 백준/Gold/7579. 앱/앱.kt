ff805f0532ed0d76eff378277cdcf0c9e7b7235d
hodak <79569951+jlkim909@users.noreply.github.com>
2024-07-15 21:39:47 +0900

    [Gold III] Title: 앱, Time: 164 ms, Memory: 21892 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.kt
+++ 백준/Gold/7579. 앱/앱.kt
@@ -2,26 +2,21 @@ package date_24_7
 
 fun main() = with(System.`in`.bufferedReader()){
     val (n, m) = readLine().split(" ").map{it.toInt()}
-    val d = Array(n){
-        IntArray(10001)
-    }
+    val d = LongArray(10001)
 
-    val memory = readLine().split(" ").map{it.toInt()}
+    val memory = readLine().split(" ").map{it.toLong()}
     val cost = readLine().split(" ").map{it.toInt()}
 
-    d[0][cost[0]] = memory[0]
-
-    for(i in 1..<n){
-        for(j in 0..<10001){
-            d[i][j] = d[i-1][j]
+    for(i in 0..<n){
+        for(j in 10000 downTo 0){
             if(j-cost[i] >= 0){
-                d[i][j] = maxOf(d[i][j], d[i-1][j-cost[i]] + memory[i])
+                d[j] = maxOf(d[j], d[j-cost[i]] + memory[i])
             }
         }
     }
 
-    for(i in 0..10001){
-        if(d[n-1][i] >= m){
+    for(i in 0..<10001){
+        if(d[i] >= m){
             print(i)
             return
         }
